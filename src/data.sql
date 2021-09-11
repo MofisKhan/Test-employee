a8957f26026621a255591d84dd1268f78edd569a
MofisKhan <mofis.khan14@gmail.com>
2021-09-12 01:52:22 +0530

    adding database sql file to the code base

---
+++ src/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+   first_name varchar(20) DEFAULT NULL,
+   last_name varchar(20) DEFAULT NULL,
+   username varchar(250) DEFAULT NULL,
+   password varchar(20) DEFAULT NULL,
+   address varchar(45) DEFAULT NULL,
+   contact varchar(45) DEFAULT NULL
+);
